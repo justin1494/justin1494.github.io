f915a81d01cc45b091873e0819859c6f1caeb517
justin1494 <m.jaskolowski1994@gmail.com>
2022-06-06 22:20:37 +0200

    project 2 added

---
+++ Projekt 2 - Kalkulator rachunków i napiwków/script.js
@@ -0,0 +1,23 @@
+const priceInput = document.querySelector("#price");
+const peopleInput = document.querySelector("#people");
+const tip = document.querySelector("#tip");
+const calcBtn = document.querySelector(".count");
+const error = document.querySelector(".error");
+const costInfo = document.querySelector(".cost-info");
+const cost = document.querySelector(".cost");
+
+const getBill = () => {
+  if (priceInput.value && peopleInput.value) {
+    error.textContent = "";
+    const price = parseFloat(priceInput.value);
+    const people = parseFloat(peopleInput.value);
+    const tipValue = parseFloat(tip.value);
+
+    cost.textContent = ((price + price * tipValue) / people).toFixed(2);
+    costInfo.style.display = "block";
+  } else {
+    error.textContent = "Proszę wypełnić wszystkie pola";
+  }
+};
+
+calcBtn.addEventListener("click", getBill);
