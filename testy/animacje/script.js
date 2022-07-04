ccb35c21e59c56c3b3479023a26629d82fa97db3
justin1494 <m.jaskolowski1994@gmail.com>
2022-07-04 21:24:09 +0200

    bug fixes

--- testy/animacje/script.js
+++ testy/animacje/script.js
@@ -1,3 +1,14 @@
-const laptopGuy = document.querySelector(".laptop-guy");
+// function squareDigits(num) {
+//   const string = num.toString();
+//   const digitArray = [];
+//   for (let i = 0; i < string.length; i++) {
+//     digitArray.push(Math.pow(string.slice(i, i + 1), 2).toString());
+//   }
+//   digitArray.toString().replaceAll(",", "");
+// }
 
-console.log(laptopGuy);
+// squareDigits(3212);
+
+let dateInput = document.querySelector(".date");
+
+console.log(dateInput.value);
